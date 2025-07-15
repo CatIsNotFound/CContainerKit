c7323d43978d867c6b73b871f8aba424b8b24985
CatIsNotFound <liaoxinkai0408@outlook.com>
2025-07-16 01:11:00 +0800

    07/16: Improve CDebug related functions to support the output of different levels of information.

---
+++ include/CContainerKit.h
@@ -0,0 +1,12 @@
+
+#pragma once
+#ifndef CVECTORSUITE_CVECTORSUITE_H
+#define CVECTORSUITE_CVECTORSUITE_H
+#include "CContainerKit/CVariant.h"
+#include "CContainerKit/CDebug.h"
+#include "CContainerKit/CVector.h"
+#include "CContainerKit/CString.h"
+
+
+
+#endif //CVECTORSUITE_CVECTORSUITE_H
